877f7dece39483c5b33a6cb7aa0cdc10b9faa91f
Jayanta Mondal <jayanta551905@gmail.com>
2022-11-09 12:58:08 +0600

    initial commit

---
+++ _noteDataType.js
@@ -0,0 +1,17 @@
+// data type 
+/**
+There are two data type in Java script
+1. primitive data type
+2. Non primitive data type
+
+// primitive data type
+primitive data type stored values
+
+// Non primitive data type
+Non primitive data type not stored values. But save value reference
+Like: 1.  array
+      2.  object
+      3.  function
+
+
+ */
